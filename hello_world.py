f932c9e65c16bb6fc1b59fdb1bf43d9a70a54494
Gourab <smlgourab@gmail.com>
2024-11-07 01:34:22 +0530

    Basic Code

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
-print("My First Python Script!!")
+#My First Program#
+name = input("What is your name?")
+print("This is my first Python Script!! & My name is "+ name)
+
+# Python Keywords - True, False, None, continue, del, pass
+
